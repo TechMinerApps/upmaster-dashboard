4fb23f8751c381a14611a4328ee4434fe1ca80a4
hongfanmeng <hongfanmeng@gmail.com>
2021-02-04 11:09:56 +0800

    (feat) layout: add a drawer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <nav-drawer />
     <nav-bar />
     <v-main class="grey lighten-3">
       <router-view />
@@ -9,8 +10,9 @@
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import NavDrawer from "./components/NavDrawer.vue";
 export default {
-  components: { NavBar },
+  components: { NavBar, NavDrawer },
   name: "App",
   data: () => ({})
 };
