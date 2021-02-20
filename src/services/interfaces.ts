38515394f799722b2a573a0c54b2dc356d89742a
truco <22969604+truc0@users.noreply.github.com>
2021-02-20 00:43:51 -0500

    feat: add interfaces declaration

---
+++ src/services/interfaces.ts
@@ -0,0 +1,41 @@
+interface Base {
+  readonly id: number;
+}
+
+interface Alert extends Base {
+  alert_channal?: AlertChannal;
+  alert_channal_id: number;
+  status: number;
+  user?: User;
+  user_id: number;
+}
+
+interface AlertChannal extends Base {
+  alerts?: Array<Alert>;
+  user?: User;
+  user_id: number;
+  is_enabled: boolean;
+  type: number;
+  config: string;
+}
+
+interface Endpoint extends Base {
+  name: string;
+  user?: User;
+  user_id: number;
+  url: string;
+  interval: number;
+  is_enabled: boolean;
+  is_public: boolean;
+  alerts?: Array<Alert>;
+}
+
+interface User extends Base {
+  email: string;
+  username: string;
+  password: string;
+  is_admin: boolean;
+  endpoints?: Array<Endpoint>;
+  alerts?: Array<Alert>;
+  alert_channals?: Array<AlertChannal>;
+}
